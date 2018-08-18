c5ad7f11e2cb768018c8a0ec850a615cef943c7a
vamsiarisetti <32108368+vamsiarisetti@users.noreply.github.com>
2018-08-19 00:22:03 +0530

    Logging in Files and accessing via weebpage
    Signed-off-by: vamsiarisetti <32108368+vamsiarisetti@users.noreply.github.com>

--- webapp/wenapptest.py
+++ webapp/wenapptest.py
@@ -24,27 +24,42 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4Letters(phrase, letters))
-    logReq = log_request(request,results,phrase, letters)
+    logReq = log_request(request,results)
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
                            the_letters=letters,
                            the_results=results)
 
-
 @app.route ('/entry')
 def entry_page()-> 'html':
     return render_template('entry.html',the_title = 'welcome to search4letters page')
 
-def log_request(req: 'flask_request', res: str,phrase, letters) -> None:
+def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log','a') as log:
-        print(req, "Letters Found : "+res, "Given phrase : "+phrase, "Letters to be Found : "+letters, file=log)
+        # print(str(dir(req)), "Letters Found : "+res, "Given phrase : "+phrase, "Letters to be Found : "+letters, file=log)
+        # print(req.form, file=log, end='|')           # get the data from the HTML form
+        # print(req.remote_addr, file=log, end='|')    # get the IP Address
+        # print(req.user_agent, file=log, end='|')     # Browser details
+        # print(res, file=log)                         # Response
+
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/viewLog')
-def view_log() -> str:
+def view_log() -> 'html':
+    content = []
     with open('vsearch.log') as log:
-        content = escape(log.read());
-    return "===>"+content
+        for line in log:
+            content.append([])
+            for lineSplit in line.split('|'):
+                content[-1].append(escape(lineSplit))
+    formHdrs = ['Form Data', 'Remote_addr', 'User_agent', 'Results']
+    return render_template('viewlog.html',
+                           the_title='View logs Here',
+                           the_row_titles=formHdrs,
+                           the_data=content,)
 
 # app.run(debug=True, host='192.168.0.108')
-app.run(host='192.168.0.102')
+
+# if __name__ == '__main__':
+app.run(debug=True)
